2d91caaa6dfdf2d4322bc0f9874c14185f0952d4
Haider Malik <haidermalik504@gmail.com>
2018-08-27 15:56:31 +0500

    create apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { gql } from 'apollo-boost';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+});
+const coursesRawQuery = gql`
+  {
+    courseFeed {
+      id
+      description
+      name
+    }
+  }
+`;
+console.log(coursesRawQuery);
+
+client
+  .query({
+    query: gql`
+      {
+        courseFeed {
+          id
+          description
+          name
+        }
+      }
+    `
+  })
+  .then(results => console.log(results));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
