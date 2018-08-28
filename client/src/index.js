3c8926ab4424bb3f66ecca6e86b6c384e8215dd3
Haider Malik <haidermalik504@gmail.com>
2018-08-28 11:11:38 +0500

    connect client to react

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import './index.css';
 import App from './App';
@@ -33,5 +34,10 @@ client
     `
   })
   .then(results => console.log(results));
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
