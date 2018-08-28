967125867f2efeb5728c9ee8f1ac4dc752447070
Haider Malik <haidermalik504@gmail.com>
2018-08-28 12:25:26 +0500

    fetch data using react apollo query

---
+++ client/src/Courses.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+const renderCourses = ({ data, error, loading }) => {
+  if (loading) return <p>...Loading</p>;
+  if (error) return <p>Error</p>;
+  return data.courseFeed.map(({ id, description, name, isPublished }) => {
+    return (
+      <div key={id}>
+        <p>Name: {name} </p>
+        <p>Description: {description} </p>
+      </div>
+    );
+  });
+};
+const Courses = () => {
+  return (
+    <div>
+      <Query
+        query={gql`
+          {
+            courseFeed {
+              id
+              description
+              name
+              isPublished
+            }
+          }
+        `}
+      >
+        {({ data, error, loading }) => {
+          if (loading) return <p>...Loading</p>;
+          if (error) return <p>Error</p>;
+          return data.courseFeed.map(
+            ({ id, description, name, isPublished }) => {
+              return (
+                <div key={id}>
+                  <p>Name: {name} </p>
+                  <p>Description: {description} </p>
+                </div>
+              );
+            }
+          );
+        }}
+      </Query>
+    </div>
+  );
+};
+
+export default Courses;
--- client/src/index.js
+++ client/src/index.js
@@ -10,30 +10,6 @@ import registerServiceWorker from './registerServiceWorker';
 const client = new ApolloClient({
   uri: 'http://localhost:4000'
 });
-const coursesRawQuery = gql`
-  {
-    courseFeed {
-      id
-      description
-      name
-    }
-  }
-`;
-console.log(coursesRawQuery);
-
-client
-  .query({
-    query: gql`
-      {
-        courseFeed {
-          id
-          description
-          name
-        }
-      }
-    `
-  })
-  .then(results => console.log(results));
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
