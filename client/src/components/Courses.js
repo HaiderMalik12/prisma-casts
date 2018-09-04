d3049e96eb04517d46e64a5644836271593dbafe
Haider Malik <haidermalik504@gmail.com>
2018-09-04 09:41:24 +0500

    add pagination controls

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -15,58 +15,77 @@ class Courses extends React.Component {
           {({ data, error, loading }) => {
             if (loading) return <Spinner />;
             if (error) return <ErrorMessage error={error} />;
-            return data.courseFeed.map(
-              ({ id, description, name, isPublished }) => {
-                return (
-                  <div key={id} className="card container">
-                    <div className="card-body">
-                      <h5 className="card-title">{name}</h5>
-                      <p className="card-text"> {description} </p>
-                      {authToken ? (
-                        <React.Fragment>
-                          <Link
-                            to={`course/${id}/edit`}
-                            className="btn btn-primary"
-                          >
-                            Edit
-                          </Link>
-                          <Mutation
-                            mutation={DELETE_COURSE_MUTATION}
-                            variables={{ id }}
-                            update={(cache, { data: { deleteCourse } }) => {
-                              const { courseFeed } = cache.readQuery({
-                                query: COURSE_FEED_QUERY
-                              });
-                              cache.writeQuery({
-                                query: COURSE_FEED_QUERY,
-                                data: {
-                                  courseFeed: courseFeed.filter(
-                                    course => course.id !== deleteCourse.id
-                                  )
-                                }
-                              });
-                            }}
-                          >
-                            {(deleteCourse, { data, error, loading }) => {
-                              return (
-                                <button
-                                  style={{ marginLeft: '10px' }}
-                                  className="btn btn-danger"
-                                  onClick={async () => {
-                                    await deleteCourse();
-                                  }}
-                                >
-                                  Delete
-                                </button>
-                              );
-                            }}
-                          </Mutation>
-                        </React.Fragment>
-                      ) : null}
-                    </div>
-                  </div>
-                );
-              }
+            return (
+              <React.Fragment>
+                {data.courseFeed.courses.map(
+                  ({ id, description, name, isPublished }) => {
+                    return (
+                      <div key={id} className="card container">
+                        <div className="card-body">
+                          <h5 className="card-title">{name}</h5>
+                          <p className="card-text"> {description} </p>
+                          {authToken ? (
+                            <React.Fragment>
+                              <Link
+                                to={`course/${id}/edit`}
+                                className="btn btn-primary"
+                              >
+                                Edit
+                              </Link>
+                              <Mutation
+                                mutation={DELETE_COURSE_MUTATION}
+                                variables={{ id }}
+                                update={(cache, { data: { deleteCourse } }) => {
+                                  const { courseFeed } = cache.readQuery({
+                                    query: COURSE_FEED_QUERY
+                                  });
+                                  cache.writeQuery({
+                                    query: COURSE_FEED_QUERY,
+                                    data: {
+                                      courseFeed: courseFeed.filter(
+                                        course => course.id !== deleteCourse.id
+                                      )
+                                    }
+                                  });
+                                }}
+                              >
+                                {(deleteCourse, { data, error, loading }) => {
+                                  return (
+                                    <button
+                                      style={{ marginLeft: '10px' }}
+                                      className="btn btn-danger"
+                                      onClick={async () => {
+                                        await deleteCourse();
+                                      }}
+                                    >
+                                      Delete
+                                    </button>
+                                  );
+                                }}
+                              </Mutation>
+                            </React.Fragment>
+                          ) : null}
+                        </div>
+                      </div>
+                    );
+                  }
+                )}
+                <nav aria-label="Page navigation example">
+                  <ul class="pagination justify-content-center">
+                    <li class="page-item">
+                      <div class="page-link" style={{ cursor: 'pointer' }}>
+                        Previous
+                      </div>
+                    </li>
+
+                    <li class="page-item">
+                      <div class="page-link" style={{ cursor: 'pointer' }}>
+                        Next
+                      </div>
+                    </li>
+                  </ul>
+                </nav>
+              </React.Fragment>
             );
           }}
         </Query>
@@ -86,10 +105,13 @@ export const DELETE_COURSE_MUTATION = gql`
 export const COURSE_FEED_QUERY = gql`
   {
     courseFeed {
-      id
-      description
-      name
-      isPublished
+      count
+      courses {
+        id
+        description
+        name
+        isPublished
+      }
     }
   }
 `;
