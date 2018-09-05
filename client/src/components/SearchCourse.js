894a76eea063e9edaabe8643feb7656851e6a1c7
Haider Malik <haidermalik504@gmail.com>
2018-09-05 10:11:20 +0500

    search courses

--- client/src/components/SearchCourse.js
+++ client/src/components/SearchCourse.js
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import { gql } from 'apollo-boost';
 import { withApollo } from 'react-apollo';
 import PropTypes from 'prop-types';
-import Course from './Course';
+import Spinner from './Spinner/Spinner';
 
 class SearchCourse extends Component {
   state = {
     filter: '',
-    courses: []
+    courses: [],
+    loading: false
   };
   executeSearch = async e => {
     e.preventDefault();
     const { client } = this.props;
     const { filter } = this.state;
+    this.setState({ loading: true });
     const { data } = await client.query({
       query: SEARCH_QUERY,
       variables: {
         filter
       }
     });
-    this.setState({ courses: data.courseFeed.courses });
+    this.setState({ courses: data.courseFeed.courses, loading: false });
   };
   render() {
     return (
@@ -41,8 +43,16 @@ class SearchCourse extends Component {
             </div>
           </div>
         </form>
-        {this.state.courses.map(course => {
-          return <Course course={course} key={course.id} />;
+        {this.state.loading && <Spinner />}
+        {this.state.courses.map(({ name, description }) => {
+          return (
+            <div className="card container">
+              <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text"> {description} </p>
+              </div>
+            </div>
+          );
         })}
       </div>
     );
